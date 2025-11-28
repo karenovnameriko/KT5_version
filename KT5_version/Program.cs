818fcf6df884d0d58439c1af86462c30f1e8dd94
karenovnameriko <meri.terteryan.2007@mail.ru>
2025-11-28 20:25:26 +0300

    Initial commit with version 1.0.0

---
+++ KT5_version/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace KT5_version;
+
+class Program
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Пользователь: {Name}, Возраст: {Age}");
+        }
+
+        public bool IsAdult()
+        {
+            return Age >= 18;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string version = "unknown";
+        if (File.Exists("version.txt"))
+        {
+            string[] lines = File.ReadAllLines("version.txt");
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("Current="))
+                {
+                    version = line.Split('=')[1].Trim();
+                    break;
+                }
+            }
+        }
+        User user1 = new User("Иван", 25);
+        User user2 = new User("Анна", 16);
+
+        user1.DisplayInfo();
+        Console.WriteLine($"Совершеннолетний: {user1.IsAdult()}");
+
+        user2.DisplayInfo();
+        Console.WriteLine($"Совершеннолетняя: {user2.IsAdult()}");
+
+    }
+}
+
